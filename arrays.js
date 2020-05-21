fa195aae3cd644b75c40877c669a88ff68e8373d
David MacPhail <david.macphail@icloud.com>
2020-05-21 17:29:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers, hundred grand, kitkat, and skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  ['element', ...[1]
+  ['element', ...[1]]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
@@ -10,4 +10,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 
 function addElementToEndOfArray(array, element){
   [...[array], 'element']
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element)
 }
