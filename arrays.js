65200bf189124fb4be395b456076bd3725c4fd92
David MacPhail <david.macphail@icloud.com>
2020-05-21 16:11:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  const chocolateBars = ['foo', ...chocolateBars]
+  chocolateBars = [...chocolateBars, 'foo']
 }
 
 function addElementToEndOfArray(array, element) {
