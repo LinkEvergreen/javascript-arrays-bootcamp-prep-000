125c84ad9dd9619fe733eb9e432faab08d2d6deb
David MacPhail <david.macphail@icloud.com>
2020-05-21 16:57:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,5 @@
 var chocolateBars = ['snickers, hundred grand, kitkat, and skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  ([1],'element') = ['element', 1]
-  return ['element', 1]
+  return array.upshift("element")
 }
